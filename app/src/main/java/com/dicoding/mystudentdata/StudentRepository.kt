c5304739a80a7688b4cfe29e3dd7cb5c3985427d
ArtForYou <kakakunyukya@gmail.com>
2023-10-23 09:18:29 +0700

    initial Commit

---
+++ app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
@@ -0,0 +1,28 @@
+package com.dicoding.mystudentdata
+
+import androidx.lifecycle.LiveData
+import com.dicoding.mystudentdata.database.Student
+import com.dicoding.mystudentdata.database.StudentAndUniversity
+import com.dicoding.mystudentdata.database.StudentDao
+import com.dicoding.mystudentdata.database.StudentWithCourse
+import com.dicoding.mystudentdata.database.UniversityAndStudent
+import com.dicoding.mystudentdata.helper.InitialDataSource
+import com.dicoding.mystudentdata.helper.SortType
+import com.dicoding.mystudentdata.helper.SortUtils
+
+class StudentRepository(private val studentDao: StudentDao) {
+    fun getAllStudent(sortType: SortType): LiveData<List<Student>> {
+        val query = SortUtils.getSortedQuery(sortType)
+        return studentDao.getAllStudent(query)
+    }
+    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentDao.getAllStudentAndUniversity()
+    fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> = studentDao.getAllUniversityAndStudent()
+    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> = studentDao.getAllStudentWithCourse()
+
+//    suspend fun insertAllData() {
+//        studentDao.insertStudent(InitialDataSource.getStudents())
+//        studentDao.insertUniversity(InitialDataSource.getUniversities())
+//        studentDao.insertCourse(InitialDataSource.getCourses())
+//        studentDao.insertCourseStudentCrossRef(InitialDataSource.getCourseStudentRelation())
+//    }
+}
---
+++ app/src/main/java/com/dicoding/mystudentdata/helper/SortUtils.kt
@@ -0,0 +1,22 @@
+package com.dicoding.mystudentdata.helper
+
+import androidx.sqlite.db.SimpleSQLiteQuery
+import java.lang.StringBuilder
+
+object SortUtils {
+    fun getSortedQuery(sortType: SortType): SimpleSQLiteQuery{
+        val simpleQuery = StringBuilder().append("SELECT * FROM student ")
+        when(sortType){
+            SortType.ASCENDING -> {
+                simpleQuery.append("ORDER BY name ASC")
+            }
+            SortType.DESCENDING -> {
+                simpleQuery.append("ORDER BY name DESC")
+            }
+            SortType.RANDOM -> {
+                simpleQuery.append("ORDER BY RANDOM()")
+            }
+        }
+        return SimpleSQLiteQuery(simpleQuery.toString())
+    }
+}
