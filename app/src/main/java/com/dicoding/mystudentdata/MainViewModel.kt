42bf130f1e6f88cd9470d619a377188ac57d1e5c
ArtForYou <kakakunyukya@gmail.com>
2023-10-23 09:37:50 +0700

    initial Commit

--- app/src/main/java/com/dicoding/mystudentdata/MainViewModel.kt
+++ app/src/main/java/com/dicoding/mystudentdata/MainViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagedList
 import com.dicoding.mystudentdata.database.Student
 import com.dicoding.mystudentdata.database.StudentAndUniversity
 import com.dicoding.mystudentdata.database.StudentWithCourse
@@ -25,7 +26,7 @@ class MainViewModel(private val studentRepository: StudentRepository) : ViewMode
         _sort.value = sortType
     }
 
-    fun getAllStudent(): LiveData<List<Student>> = _sort.switchMap {
+    fun getAllStudent(): LiveData<PagedList<Student>> = _sort.switchMap {
         studentRepository.getAllStudent(it)
     }
     fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentRepository.getAllStudentAndUniversity()
--- app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
+++ app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
@@ -1,6 +1,8 @@
 package com.dicoding.mystudentdata
 
 import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
 import com.dicoding.mystudentdata.database.Student
 import com.dicoding.mystudentdata.database.StudentAndUniversity
 import com.dicoding.mystudentdata.database.StudentDao
@@ -11,9 +13,17 @@ import com.dicoding.mystudentdata.helper.SortType
 import com.dicoding.mystudentdata.helper.SortUtils
 
 class StudentRepository(private val studentDao: StudentDao) {
-    fun getAllStudent(sortType: SortType): LiveData<List<Student>> {
+    fun getAllStudent(sortType: SortType): LiveData<PagedList<Student>> {
         val query = SortUtils.getSortedQuery(sortType)
-        return studentDao.getAllStudent(query)
+        val student = studentDao.getAllStudent(query)
+
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(true)
+            .setInitialLoadSizeHint(30)
+            .setPageSize(10)
+            .build()
+
+        return LivePagedListBuilder(student, config).build()
     }
     fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentDao.getAllStudentAndUniversity()
     fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> = studentDao.getAllUniversityAndStudent()
